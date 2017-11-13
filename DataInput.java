00f44b759f088bb3028022bddf977abe8a26a28c
IreneArapogiorgi <32685642+IreneArapogiorgi@users.noreply.github.com>
2017-11-14 01:35:33 +0200

    Add files via upload
    Προσπαθήστε και εσείς να τρέξετε το SpellCheckerUI και πείτε μου αν κάνει compile, γιατί ενώ εμένα μου δουλεύει κανονικά μέσω NetBeans, όταν το τρέχω στο TextPad βγαζει σφάλμα ο compiler (δεν αναγνωρίζει την DataInput). Κάποιο θέμα πρέπει να υπάρχει με το package (θα το ψάξω περισσότερο για να το διορθώσω). Επίσης, έκανα κάποιες αλλαγές στο κομμάτι του input, ώστε να μπορεί να λειτουργήσει η είσοδος δεδομένων από το χρήστη σε συνδυασμό με το GUI.

--- DataInput.java
+++ DataInput.java
@@ -1,17 +1,23 @@
-import java.util.Scanner;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.spellchecker;
+
+/**
+ *
+ * @author earapogiorgi
+ */
 public class DataInput {
-	
-	public static String[] inputFromUser() {
-		Scanner input = new Scanner(System.in);
-		String words = input.nextLine();
-		words = words.toLowerCase();
-		input.close();
-		return words.split("[^\\p{L}\\p{N}]+");
-	}
-	
-	public static String[] inputFromTxt() {
-		return null;
-		
-	}
-	
+
+    public static String[] inputFromUser(String words) {
+
+	words = words.toLowerCase();
+	return words.split("[^\\p{L}\\p{N}]+");
+    }
+    
+    public static String[] inputFromTxt() {
+	return null;
+    }
 }
