5d710c5f397e7bc6b184d757324d067c1be55b9e
Nick <nikolasea@windowslive.com>
2017-12-03 19:59:58 +0200

    Added javadoc, fixed cases where class didnt work properly and added String Builder instead of String

--- src/DataInput.java
+++ src/DataInput.java
@@ -4,15 +4,21 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-
+/**
+ * Data Input class consists of static methods and is used to:
+ * a) read plain text from a text file
+ * b) split that text- using the Word class constructor- 
+ *    from plain text to words (or numbers) and symbols to an array of Word objects
+*/
 public class DataInput {
 	
-	public static Word[] readInputFromTxt(String path, String encodingType) throws IOException, FileNotFoundException, UnsupportedEncodingException {
+	public static Word[] readInputFromTxt(String path, String encodingType) 
+			throws IOException, FileNotFoundException, UnsupportedEncodingException {
 		FileInputStream fileInputStream = new FileInputStream(path);
 		BufferedReader bufferReader = new BufferedReader(new InputStreamReader(fileInputStream, encodingType));
 		final String newLine = System.lineSeparator();
 
-		String words = "";
+		StringBuilder words = new StringBuilder();
 		String currentWord = null;
 		
 		bufferReader.mark(1); //BOM handling
@@ -20,23 +26,44 @@ public class DataInput {
 			bufferReader.reset();
 		}
 		while ((currentWord = bufferReader.readLine()) != null) {
-			words += currentWord + newLine;
+			words.append(currentWord + newLine);
 		}
 		
-		words = words.substring(0,words.length()-2); //discarding the last newline
+		words.setLength(words.length() - 2); //discarding the last newline
 		bufferReader.close();
-		return splitStringToWords(words);
+		return splitStringToWords(words.toString());
 		
 	}
 
 	private static Word[] splitStringToWords(String words) {
 		final String notWordOrNumber = "([^\\p{L}\\p{N}]+)";
-		String[] wordsArray = words.split(
-				"(?=(?!^)"+notWordOrNumber+")(?<!"+notWordOrNumber+")|(?!"+notWordOrNumber+")(?<="+notWordOrNumber+")");
-		Word[] wordsType = new Word[wordsArray.length/2];
+		Word[] wordsType;
+		int index = 0;
 		
-		for (int i = 0; i < wordsArray.length - 1; i += 2){
-			 wordsType[i/2] = new Word(wordsArray[i], wordsArray[i+1]);
+		String[] wordsArray = words.split("(?=(?!^)" + notWordOrNumber + ")(?<!" + notWordOrNumber + ")|"
+				+ "(?!" + notWordOrNumber + ")(?<=" + notWordOrNumber + ")");
+		
+		int lastWord = wordsArray.length - 1;
+		if (wordsArray[lastWord].matches(notWordOrNumber)) {
+			if (wordsArray[0].matches(notWordOrNumber)) { // if input starts with a symbol wordsType array should be longer
+				wordsType = new Word[wordsArray.length / 2 + 1];
+			} else {
+				wordsType = new Word[wordsArray.length / 2];
+			}
+		} else { // if input text ends with a letter
+			wordsType = new Word[wordsArray.length / 2 + 1];
+			wordsType[wordsType.length - 1] = new Word(wordsArray[lastWord], "");
+			lastWord--;
+		}
+		if (wordsArray[0].matches(notWordOrNumber)) { // if input starts with a symbol
+			wordsType[0] = new Word("", wordsArray[0]);
+			for (index++; index < lastWord; index += 2){
+				 wordsType[index / 2 + 1] = new Word(wordsArray[index], wordsArray[index + 1]);
+			}
+		} else {
+			for (; index < lastWord; index += 2){
+				wordsType[index / 2] = new Word(wordsArray[index], wordsArray[index + 1]);
+			}
 		}
 		return wordsType;
 	}
