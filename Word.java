95a4dbe8f9ac30aa0847d164335b2ae494604ae1
Georgios Mourikis <t8160072@dias.aueb.gr>
2018-01-10 00:35:30 +0200

    Update Word.java

--- Word.java
+++ Word.java
@@ -13,12 +13,13 @@ public class Word {
 	private static int index; // number of word orderly in the sentence
 
 	/**
-	 *field to be used in existsInDictionary and findSuggestions in
-	 *order to connect to the MySQL database
-	 *@see existsInDictionary(String)
-	 *@see findSuggestions()
+	 * field to be used in existsInDictionary and findSuggestions in order to
+	 * connect to the MySQL database
+	 * 
+	 * @see existsInDictionary(String)
+	 * @see findSuggestions()
 	 */
-	
+
 	private int count;
 
 	private String word;
@@ -28,16 +29,21 @@ public class Word {
 	private String[] bestPossibleSolutions = new String[3];
 
 	private String dividingChars;
-	
+
 	private static Pattern pattern = Pattern.compile("\\d+");
 
 	/**
 	 * Class Constructor
-	 * <p> When creating an object, the index variable gets increased by one and its value
-	 * is inserted in the count value, thus the (static) index variable shows the number<br>
+	 * <p>
+	 * When creating an object, the index variable gets increased by one and its
+	 * value is inserted in the count value, thus the (static) index variable shows
+	 * the number<br>
 	 * of objects created and the count shows the ascending number of each object.
-	 * @param inputWord - the word (or number) of the object
-	 * @param dividingChars - the symbols that follow the word
+	 * 
+	 * @param inputWord
+	 *            - the word (or number) of the object
+	 * @param dividingChars
+	 *            - the symbols that follow the word
 	 * @throws SQLException
 	 */
 	public Word(String inputWord, String dividingChars) throws SQLException {
@@ -47,56 +53,60 @@ public class Word {
 	}
 
 	/**
-	 * Method that checks if a String is a number, so as to not
-	 * check if numbers exist in the database.
-	 * @param str input String
+	 * Method that checks if a String is a number, so as to not check if numbers
+	 * exist in the database.
+	 * 
+	 * @param str
+	 *            input String
 	 * @return
 	 */
-	public static boolean isΑNumber(String str) {
-		return pattern.matcher(str).matches();
+	public boolean isΑNumber() {
+		return pattern.matcher(this.getWord()).matches();
 	}
-	
-	/**
- 	 * Method that checks if the String of the given word is the first word of the sentence the user wrote.
- 	 * it takes as input a String with the word the user gave and its count in the List of Words that holds the full text
- 	 * it is checked both from whether count is equal to 1 
- 	 * AND from wether the dividingChars of the previous Word object in the list are such that declare the end of the sentence.
- 	 * in the mentioned cases the method returns true, otherwise it returns false
- 	 * the usage of this method is needed to determine how the word will be later processed 
- 	 * @param input input String , count input int
- 	 * @return boolean
- 	 */
-	
-	public static boolean isFirstWord(String input, int count) {
-	/*	Word last = DataInput.wordsList.get(count - 1);
-		char lchar = last.dividingChars.charAt(last.dividingChars.length()); */
-		if (count == 1) {
+
+	public boolean isFirstWord() {
+		int count = this.getCount();
+		if (count <= 1) {
 			return true;
-		}/* else if (lchar != '.' && lchar != '!'&& lchar != ';') { 
-			return true;
-		} */else {
-			return false;
+		} else {
+			if (SpellCheckerUI.wordsList.get(count - 1).getDividingChars().matches("[;!.]")) {
+				return true;
+			} else {
+				return false;
+			}
 		}
 	}
 
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
 	/**
-	 * Method that calls the existInDictionary and findSuggestions methods accordingly
-	 * <p>If the input is too long and not found in the database, the largest words of the database
-	 * are used as suggestions. In case of the String being a number,<br> then it is considered correct
-	 * and if it is the first word of a sentence it is checked with both an upper case and an lower case
-	 * first letter.<br> If the previous cases are not met, and the word is not found in the database, then
-	 * the findSuggestions method is called.
+	 * Method that calls the existInDictionary and findSuggestions methods
+	 * accordingly
+	 * <p>
+	 * If the input is too long and not found in the database, the largest words of
+	 * the database are used as suggestions. In case of the String being a
+	 * number,<br>
+	 * then it is considered correct and if it is the first word of a sentence it is
+	 * checked with both an upper case and an lower case first letter.<br>
+	 * If the previous cases are not met, and the word is not found in the database,
+	 * then the findSuggestions method is called.
+	 * 
 	 * @throws SQLException
 	 */
-	
+
 	public void wordProcessing() throws SQLException {
 		String input = this.word;
-		int count = this.count;
 		if (input.length() == 0) {
 			this.isSpelledCorrectly = true;
-		} else if (input.length() > 25 ) {
-			 
-			if (input.length() <= 27 ) {
+		} else if (input.length() > 25) {
+
+			if (input.length() <= 27) {
 				this.isSpelledCorrectly = existsInDictionary(input);
 			} else {
 				this.isSpelledCorrectly = false;
@@ -106,12 +116,38 @@ public class Word {
 				this.bestPossibleSolutions[1] = "στρογγυλοκουλουριαζόσασταν";
 				this.bestPossibleSolutions[2] = "στρογγυλοκουλουριαζόντουσαν";
 			}
-		} else if (isΑNumber(input)) {
+		} else if (this.isΑNumber()) {
 			this.isSpelledCorrectly = true;
-		} else if (isFirstWord(input, count)) {
+		} else if (this.isFirstWord()) {
 			this.isSpelledCorrectly = (existsInDictionary(input) || existsInDictionary(input.toLowerCase()));
+			if (this.isSpelledCorrectly && input.equals(input.toLowerCase())) {
+				this.isSpelledCorrectly = false;
+				if (input.length() == 1) {
+					this.bestPossibleSolutions[0] = input.toUpperCase();
+					this.bestPossibleSolutions[1] = input.toUpperCase();
+					this.bestPossibleSolutions[2] = input.toUpperCase();
+
+				} else {
+					this.bestPossibleSolutions[0] = input.substring(0, 1).toUpperCase().concat(input.substring(1));
+					this.bestPossibleSolutions[1] = input.substring(0, 1).toUpperCase().concat(input.substring(1));
+					this.bestPossibleSolutions[2] = input.substring(0, 1).toUpperCase().concat(input.substring(1));
+				}
+			}
 			if (this.isSpelledCorrectly == false) {
 				this.findSuggestions();
+				if (input.length() == 1) {
+					this.bestPossibleSolutions[0] = this.bestPossibleSolutions[0].toUpperCase();
+					this.bestPossibleSolutions[1] = this.bestPossibleSolutions[1].toUpperCase();
+					this.bestPossibleSolutions[2] = this.bestPossibleSolutions[2].toUpperCase();
+
+				} else {
+					this.bestPossibleSolutions[0] = this.bestPossibleSolutions[0].substring(0, 1).toUpperCase()
+							.concat(this.bestPossibleSolutions[0].substring(1));
+					this.bestPossibleSolutions[1] = this.bestPossibleSolutions[1].substring(0, 1).toUpperCase()
+							.concat(this.bestPossibleSolutions[1].substring(1));
+					this.bestPossibleSolutions[2] = this.bestPossibleSolutions[2].substring(0, 1).toUpperCase()
+							.concat(this.bestPossibleSolutions[2].substring(1));
+				}
 			}
 		} else {
 			this.isSpelledCorrectly = existsInDictionary(input);
@@ -121,18 +157,21 @@ public class Word {
 		}
 	}
 
-	/** 
+	/**
 	 * Method used to check if a word exists in the dictionary of the database.
 	 * <p>
 	 * Method existsInDictionary receives the input, connects to the MySQL database,
-	 * in order to use a MySQL Stored Procedure<br> and search for the word in the database.
-	 * If a result is returned from the stored procedure, then the word has been found.
+	 * in order to use a MySQL Stored Procedure<br>
+	 * and search for the word in the database. If a result is returned from the
+	 * stored procedure, then the word has been found.
 	 * 
-	 * @param input - the word being searched
+	 * @param input
+	 *            - the word being searched
 	 * @return boolean result based on whether the word is spelled correctly
 	 * @throws SQLException
 	 */
 	public boolean existsInDictionary(String input) throws SQLException {
+		try {
 			CallableStatement cStmt = SpellCheckerUI.myConn.prepareCall("{call existsindictionary(?)}");
 			cStmt.setString(1, input);
 			cStmt.execute();
@@ -141,23 +180,14 @@ public class Word {
 			} else {
 				return false;
 			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			return false;
+		}
 	}
-	/**
-	 * Finds Alternatives of the user's misspelled words
-	 * <p>
-	 * findSuggestions connects with MySQL in order to execute a Stored Procedure
-	 * that through the Levenshtein Distance algorithm finds 3 or less alternative
-	 * suggestions of the word that the user misspelled. <br>
-	 * Afterwards, it inserts the suggestions into a table and closes the ResultSet
-	 * and CallableStatement that were used through the execution of the stored
-	 * procedure
-	 * 
-	 * @see ResultSet
-	 * @see CallableStatement
-	 * 
-	 * @throws SQLException
-	 */
+
 	public void findSuggestions() throws SQLException {
+		try {
 			CallableStatement cStmt = SpellCheckerUI.myConn.prepareCall("{call findsuggestions(?)}");
 			cStmt.setString(1, this.word);
 			cStmt.execute();
@@ -171,6 +201,9 @@ public class Word {
 			bestPossibleSolutions = str;
 			rs.close();
 			cStmt.close();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
 	}
 
 	public String getWord() {
@@ -204,7 +237,7 @@ public class Word {
 	public void setBestPossibleSolutions(String[] bestPossibleSolutions) {
 		this.bestPossibleSolutions = bestPossibleSolutions;
 	}
-	
+
 	@Override
 	public String toString() {
 		return word + dividingChars;
